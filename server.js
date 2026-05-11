1a3a78c81ada9dcca8c8f802d0016195703ad82c
GHOSTEMANE <78034042+GHOSTEMANEE@users.noreply.github.com>
2026-05-11 17:55:00 +0300

    add root route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("AI Server Running");
+});
+
 app.post("/chat", async (req, res) => {
 
   try {
